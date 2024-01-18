39939f9cef4396ec32034851ea0a6685d4672e03
J M Adis <jessicaadis7@gmail.com>
2024-01-18 11:52:17 +0000

    have working api call and search function

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,60 @@
 import "./App.css";
-import { useState, FormEvent, useEffect } from "react";
+import { useState, FormEvent, useEffect, ChangeEvent } from "react";
 import { Beer } from "./types/types";
-import Main from "./containers/Main/Main";
-import NavBar from "./containers/NavBar/NavBar";
-import BeerInfo from "./containers/BeerInfo/BeerInfo";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredBeers, setFilteredBeers] = useState<Beer[]>([]);
+  const [apiBeers, setApiBeers] = useState<Beer[]>([]);
 
-export default App
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://api.punkapi.com/v2/beers");
+      const data = await response.json();
+      setApiBeers(data)
+      setFilteredBeers(data)
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleInputChanges = (e: ChangeEvent<HTMLInputElement>) => {
+    const searchedTerm = e.target.value;
+
+    setSearchTerm(searchedTerm);
+
+    const filteredBevs = apiBeers.filter((beer) =>
+      beer.name.toLowerCase().includes(searchedTerm.toLowerCase())
+    );
+
+    setFilteredBeers(filteredBevs);
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleInputChanges}
+          placeholder="Type to search beers"
+        />
+      </div>
+      {filteredBeers.length === 0 ? (
+        <p>No beers found</p>
+      ) : (
+        <ol>
+          {filteredBeers.map((beer) => (
+            <li key={beer.id}>{beer.name}</li>
+          ))}
+        </ol>
+      )}
+    </div>
+  );
+};
+
+export default App;
